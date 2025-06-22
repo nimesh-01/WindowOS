8222895f64d881335d7e4757bc07cdaef123cdfd
user.email "nimeshsolanki106@gmail.com <nimeshsolanki106@gmail.com>
2025-06-22 14:33:24 +0530

    first commit

---
+++ js/Filesystem.js
@@ -0,0 +1,77 @@
+function setFolders() {
+    console.log("chala");
+    initialdata()
+    let d = JSON.parse(localStorage.getItem("data"))
+    let m = document.getElementById('main');
+    document.addEventListener("click", (e) => {
+        console.log("Ok");
+        if (!e.target.closest(".items")) {
+            document.querySelectorAll(".items").forEach(item => {
+                item.style.backgroundColor = "transparent"
+            })
+        }
+    })
+    d.forEach(el => {
+        let div = document.createElement("div")
+        let img = document.createElement("img")
+        let innerdiv = document.createElement("div")
+        div.className = "items"
+        img.src = el.url
+        img.alt = el.name + "image"
+
+        innerdiv.innerText = (el.name).slice(0, 15);
+
+        div.addEventListener("click", (obj) => {
+            // console.log(obj);
+            document.querySelectorAll(".items").forEach(item => {
+                item.style.backgroundColor = "transparent"
+            })
+            Object.assign(div.style, {
+                backgroundColor: "rgba(202, 202, 202, 0.14)"
+            });
+        })
+
+        Object.assign(img.style, {
+            width: "50px",
+            height: "50px",
+            objectFit: "cover",
+            borderRadius: "10px"
+        });
+
+
+        Object.assign(div.style, {
+            width: "70px",
+            margin: "5px",
+            padding: "5px",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "0px",
+            boxShadow: "0 2px 8px rgba(10, 10, 10, 0.14)",
+            borderRadius: "5px"
+        });
+
+        Object.assign(innerdiv.style, {
+            color: "white",
+            width: "100px",
+            padding: "5px",
+            textAlign: "center",
+            wordWrap: "break-word",
+            overflowWrap: "break-word",
+            whiteSpace: "normal",
+        });
+
+
+        innerdiv.style.color = "white"
+        innerdiv.style.width = "100%"
+        div.appendChild(img)
+        div.appendChild(innerdiv)
+        m.appendChild(div);
+    });
+
+}
+// window.setFolders=setFolders;
+function createfolder() {
+
+}
---
+++ js/Localdata.js
@@ -0,0 +1,46 @@
+function initialdata() {
+    localStorage.setItem("data", JSON.stringify([{
+        "name": "PC",
+        "url": "assests/icons/pc.png"
+    },
+    {
+        "name": "folder1folder1folder1folder1folder1",
+        "url": "assests/icons/folder.webp"
+    },{
+        "name": "PC",
+        "url": "assests/icons/pc.png"
+    },
+    {
+        "name": "folder1folder1folder1folder1folder1",
+        "url": "assests/icons/folder.webp"
+    },{
+        "name": "PC",
+        "url": "assests/icons/pc.png"
+    },
+    {
+        "name": "folder1folder1folder1folder1folder1",
+        "url": "assests/icons/folder.webp"
+    },{
+        "name": "PC",
+        "url": "assests/icons/pc.png"
+    },
+    {
+        "name": "folderer1",
+        "url": "assests/icons/folder.webp"
+    },{
+        "name": "PC",
+        "url": "assests/icons/pc.png"
+    },
+    {
+        "name": "folder1folder1folder1folder1folder1",
+        "url": "assests/icons/folder.webp"
+    },{
+        "name": "PC",
+        "url": "assests/icons/pc.png"
+    },
+    {
+        "name": "folder1folder1folder1folder1folder1",
+        "url": "assests/icons/folder.webp"
+    }
+]))
+}
